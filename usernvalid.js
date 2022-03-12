e71df9fd336adb12c368ac6b90b6c61232799ca0
KABILA Anas <93036687+flashi00@users.noreply.github.com>
2022-03-12 18:14:06 +0100

    Add files via upload

---
+++ usernvalid.js
@@ -0,0 +1,63 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const error = document.getElementById('error')
+
+localStorage.setItem('User1@gmail.com', "mypassw1")
+localStorage.setItem('User2@gmail.com', "mypassw2")
+localStorage.setItem('User3@gmail.com', "mypassw3")
+localStorage.setItem('User4@gmail.com', "mypassw4")
+
+
+function usernameIsValid(username) {
+    return /^[a-zA-Z][0-9a-zA-Z_.-]+@gmail.com+$/.test(username);
+}
+form.addEventListener('submit', (e) => {
+    let messages = []
+    let pmessages = []
+    if (username.value == '') {
+        messages.push("username is required")
+    }
+    else
+    {
+        if(!usernameIsValid(username.value)) {
+            messages.push("insert a valid email")
+        }
+    }
+
+    if (password.value == '') {
+        pmessages.push("password is required")
+    }
+    else if (password.value.length < 8) {
+        pmessages.push("password should contain at least 8 characters")
+    }
+    else if(localStorage.getItem(username.value)) {
+            if(password.value != localStorage.getItem(username.value)) {
+                pmessages.push("password is not valid")
+            }  
+        }
+    else { 
+        pmessages.push("This account dosen't exist") 
+    }
+    if(messages.length > 0) {
+        e.preventDefault()
+        error.innerHTML = messages.join(', ')
+        username.style.border = "2px solid #ff3860"
+    } else {
+        error.innerHTML = ''
+        username.style.border = "2px solid #199719";
+    }
+    if(pmessages.length > 0) {
+        e.preventDefault()
+        perror.innerHTML = pmessages.join(', ')
+        password.style.border = "2px solid #ff3860"
+    } else {
+        e.preventDefault()
+
+    perror.innerHTML = 'Success'
+    perror.style.color = "#199719"
+    perror.style.fontSize = "20px"
+    password.style.border = "2px solid #199719";
+    //window.alert('Succes')
+    }
+})
